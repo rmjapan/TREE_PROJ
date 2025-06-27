482cae050d995803d6bd8e46bda2acfc1dd8860b
Ryuichi Miyauchi <s2410178@jaist.ac.jp>
2025-06-27 13:52:10 +0900

    training_step()の作成が完了.

--- train_vqvae.py
+++ train_vqvae.py
@@ -1,30 +1,46 @@
 
 
-from model.svsae import AutoencoderConfig
+from pyexpat import model
+from model.svsae import AutoencoderConfig,SVSEncoder_ver2,SVSDecoder_ver2,VectorQuantizer
+from my_dataset.svsdataset import SvsDataLoader
 from lightning import LightningModule
+import torch.nn.functional as F
 from Octree.octfusion.datasets import dataloader
-
 
 def vq_train_loop()
 def setup_dataset
 class VQVAE(LightningModule):
     def __init__(self,model_config):
         super().__init__()
+        self.encode=SVSEncoder_ver2(model_config).to(model_config.device)
+        self.decode=SVSDecoder_ver2(model_config).to(model_config.device)
+        self.vq=VectorQuantizer(model_config,beta=0.25).to(model_config.device)
+        
+  
+        
+        
     def training_step(self,batch,batch_idx):
-        #dataloaderをまず実装する必要があるね.
+        #foward プロセス
+        x=batch
+        latent_feature=self.encode(x)
+        vq_loss,vq_output,emb=self.vq(latent_feature)
+        x_recon=self.decode(vq_output)
         
-        #損失計算    
+        #損失計算
+        recon_loss = F.mse_loss(x_recon, x)
+        total_loss = recon_loss + vq_loss   
+        
+        # ログ記録（個別の損失も記録すると分析しやすい）
+        self.log('train_total_loss', total_loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log('train_recon_loss', recon_loss, on_step=True, on_epoch=True)
+        self.log('train_vq_loss', vq_loss, on_step=True, on_epoch=True)
+        return total_loss        
         
         
         
         
-def main()
-    setup_dataset(dataset_path)
-    train_data=train_dataset
-    test_data= test_dataset
-    transforms=Mytransform
-    dataloader=Mydataloader
-    model=
-    vq_train_loop()#ここで訓練する
-    vq_test_loop()
-    vq_save_model()
+        
+def main():
+    dataloader=SvsDataLoader(batch_size=8)
+    
+    
